6c5a1e16d08ad37dea878e045ccf526f84645309
Tomoya Koyanagi <tomk79@gmail.com>
2019-02-17 21:17:04 +0900

    文字セット変換の処理を改善

--- php/path_resolver.php
+++ php/path_resolver.php
@@ -82,12 +82,11 @@ class path_resolver{
 		// もとの文字セットを記憶 → UTF-8 に一時変換 → Simple HTML Parser → 最後にもとの文字セットに変換しなおす
 		// という処理で対応した。
 		$detect_encoding = mb_detect_encoding($src);
-		$src = mb_convert_encoding( $src, DEFAULT_TARGET_CHARSET );
 
 
 		// HTMLをパース
 		$html = str_get_html(
-			$src ,
+			mb_convert_encoding( $src, DEFAULT_TARGET_CHARSET, $detect_encoding ) ,
 			false, // $lowercase
 			false, // $forceTagsClosed
 			DEFAULT_TARGET_CHARSET, // $target_charset
